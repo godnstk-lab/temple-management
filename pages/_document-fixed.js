167a552bb728845ee5a5292de0061de5a4e75d18
godnstk-lab <godnstk@gmail.com>
2025-12-11 22:31:46 +0900

    Update _document-fixed.js

--- pages/_document-fixed.js
+++ pages/_document-fixed.js
@@ -4,13 +4,10 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <meta name="application-name" content="해운사" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content="해운사" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="theme-color" content="#78350f" />
-        <meta name="msapplication-TileColor" content="#78350f" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
