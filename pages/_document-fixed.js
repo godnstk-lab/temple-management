27febe149f8395b0950ed86d42dfcc32e69020ed
godnstk-lab <godnstk@gmail.com>
2025-12-12 08:47:50 +0900

    Update _document-fixed.js

--- pages/_document-fixed.js
+++ pages/_document-fixed.js
@@ -6,9 +6,15 @@ export default function Document() {
       <Head>
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="theme-color" content="#78350f" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="해운사" />
+        <meta name="theme-color" content="#d97706" />
+        
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/icon-192.png" />
+        <link rel="apple-touch-icon" sizes="512x512" href="/icon-512.png" />
       </Head>
       <body>
         <Main />
