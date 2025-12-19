1584be75694504ea8d51bee189e65b1815ca4ab5
godnstk-lab <godnstk@gmail.com>
2025-12-19 09:18:52 +0900

    Remove next-pwa config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-});
-
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-});
+}
+
+module.exports = nextConfig
